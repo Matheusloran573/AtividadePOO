47472812758238c9ecbe180a7b31f3771ea092ea
Matheus L <129105584+Matheusloran573@users.noreply.github.com>
2023-09-27 21:53:26 -0300

    Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,9 +1,17 @@
 public class Usuario {
     String nome;
     String email;
+    double investimento;
+    double orcamento;
+    double meta;
+    String conta;
 
-    public Usuario(String nome, String email){
+    public Usuario(String nome, String email, double investimento, double orcamento, double meta, String conta){
         this.nome = nome;
         this.email = email;
+        this.investimento = investimento;
+        this.orcamento = orcamento;
+        this.meta = meta;
+        this.conta = conta;
     }
 }
