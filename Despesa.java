3dae269ca6384e2393a4ba3ec6d024f4b6384f0c
Matheus L <129105584+Matheusloran573@users.noreply.github.com>
2023-09-27 21:50:40 -0300

    Despesa.java

--- Despesa.java
+++ Despesa.java
@@ -2,10 +2,12 @@ public class Despesa {
     double valor;
     String categoria;
     String data;
+    String descricao;
 
-    public Despesa(double valor, String categoria, String data){
+    public Despesa(double valor, String categoria, String data, String descricao){
         this.valor = valor;
         this.categoria = categoria;
         this.data = data;
+        this.descricao = descricao;
     }
 }
